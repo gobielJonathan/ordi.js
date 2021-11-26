b7c68fb4f72c90ea01c3b7065f5d6f3cf69ef425
Jonathan Gobiel <jonathangobiel13@gmail.com>
2021-11-26 15:40:25 +0700

    init ssr

---
+++ packages/beyond/src/client/app.js
@@ -0,0 +1,10 @@
+import "./app.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="title">hello world from beyond</div>
+    </div>
+  );
+};
+export default App;
