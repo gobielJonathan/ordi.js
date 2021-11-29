5dd4ac0966a73134d4ca936380cad97d591fcb3e
Jonathan Gobiel <jonathangobiel13@gmail.com>
2021-11-29 14:21:40 +0700

    csr and ssr support helment support

--- packages/beyond/src/client/app.js
+++ packages/beyond/src/client/app.js
@@ -1,12 +1,20 @@
+import { Helmet } from "react-helmet-async";
 import "./app.css";
 import dummy from "./dummy.png";
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="title">hello world from beyond</div>
-      <img src={dummy} alt="dummy" />
-    </div>
+    <>
+      <Helmet>
+        <title>App</title>
+      </Helmet>
+      <div className="App">
+        <div className="title">
+          hello world from beyond changes testing from changes 12390123
+        </div>
+        <img src={dummy} alt="dummy" />
+      </div>
+    </>
   );
 };
 export default App;
