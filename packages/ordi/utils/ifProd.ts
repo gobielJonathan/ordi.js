f1d6b0f72a46c33771259948f1247e58e787e831
Jonathan Gobiel <46088334+gobielJonathan@users.noreply.github.com>
2024-08-01 23:31:39 +0700

    fix: css modules prod (#35)

--- packages/ordi/utils/ifProd.ts
+++ packages/ordi/utils/ifProd.ts
@@ -1,11 +1,11 @@
+import ifFunc from "./ifFunc";
+
 const ifProd = (_true: unknown, _false?: unknown) => {
   if (process.env.NODE_ENV === "production") {
-    if (_true instanceof Function) {
-      return _true();
-    }
-    return _true;
+    return ifFunc(_true);
   }
-  return _false;
+
+  return ifFunc(_false);
 };
 
 export default ifProd;
